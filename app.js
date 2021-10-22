41f94df5ccea3aaaf0afc4991c3983bb759e4ca8
TimTaylor122 <the.tim.taylor122@gmail.com>
2021-10-22 13:40:59 -0600

    Set up User Authorization and Experiment with SQL integration and endpoints

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const {readFileSync} = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
-const notFoundPage = readFileSync('./pages/notFound.html')
+const SqlService = require('./sqlService')
+const { auth, requiresAuth } = require('express-openid-connect')
+require('dotenv').config()
 
-app.use(express.static('./public'))
+app.use(
+    express.static('./public'),
+    auth({
+        authRequired: false,
+        auth0Logout: true,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+    })
+)
 
-app.get('/api/creature/:creatureID', (req, res) => {
-    creature = {
-        id : req.params.creatureID,
-        name : "Test Creature",
-    }
-    res.json(creature)
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged In' : 'Logged Out')
+})
+
+app.get('/api/creature/all', requiresAuth(), (req, res) => {
+    res.json(SqlService.GetAllCreatures())
+})
+
+app.get('/api/creature/:creatureId', requiresAuth(), (req, res) => {
+    res.json(SqlService.GetCreature(req.params.creatureId))
+})
+
+app.get('/api/user/creatures', requiresAuth(), (req, res) => {
+    res.json(SqlService.GetUserCreatures(req.oidc.user))
 })
 
 app.all('*', (req,res) => {
     res.status(404).send(notFoundPage)
 })
 
-app.listen(port, ()=>{console.log(`Server is listening on port ${port}...`)})
+app.listen(port, () => {console.log(`Server is listening on port ${port}...`)})
 
--- sqlService.js
+++ sqlService.js
@@ -1,16 +1,36 @@
 mysql = require('mysql')
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'me',
-    password : 'secret',
-    database : 'my_db'
-});
-
-connection.connect()
-
-connection.query('SELECT 1', function(error, results, fields) {
-    if (error) throw error
-    
+connection = mysql.createConnection({
+    host: 'localhost',
+    user: '',
+    password: '',
+    database: 'rpg',
 })
 
-connection.end()
+exports.GetAllCreatures = () => {
+    connection.query('SELECT * FROM creature', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+        return results[0].solution
+    });
+}
+
+exports.GetCreature = (creatureId) => {
+    connection.query('SELECT * FROM `creature` WHERE `creatureId` = ?', [creatureId], function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+        return results[0].solution
+    });
+}
+
+exports.GetUserCreatures = (user) => {
+    connection.query('SELECT `userId` FROM `user` WHERE `user.username` = ?', [user.name], function (error, result, fileds) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+        userId = results[0].solution
+    })
+    connection.query('SELECT * FROM `creature` JOIN `creature_access` ON `creature.creatureId` = `creature_access.creatureId` WHERE `userId` = ?', [userId], function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+        return results[0].solution
+    });
+}
