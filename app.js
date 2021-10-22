e6bca479272c37d8e446330a9ab5f1998fea3312
TimTaylor122 <the.tim.taylor122@gmail.com>
2021-10-21 23:02:10 -0600

    ?

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const {readFileSync} = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const notFoundPage = readFileSync('./pages/notFound.html')
 
 app.use(express.static('./public'))
+
+app.get('/api/creature/:creatureID', (req, res) => {
+    creature = {
+        id : req.params.creatureID,
+        name : "Test Creature",
+    }
+    res.json(creature)
+})
 
 app.all('*', (req,res) => {
     res.status(404).send(notFoundPage)
